28f212ef3f071ff2fdafa8982516a0a9acd773cb
HenrikSamuelsson <henrik.samuelsson@gmail.com>
2021-07-23 11:46:46 +0200

    Also print usingi pointers.

--- exercise-015/exercise-015.c
+++ exercise-015/exercise-015.c
@@ -3,14 +3,24 @@
 
 int main(int argc, char *argv[])
 {
-	// Create two arrays with ages and correpsonding names.
+	// Define an array holding numbers indicating ages.
 	int ages[] = {23, 43, 12, 89, 2};
+
+	// Define an array holding string indicating names. where 
+	// Each name orrespondes to the ages array, i.e Alan is 23 years old.
 	char *names[] = {"Alan", "Frank", "Mary", "John", "Lisa"};
 
-	// Get the number of elements in the array ages.
+	// Setup a pointer to the start of the ages array.
+	int *ages_p = ages;
+	
+	// Setup a pointer to pointer to char to the start of the names array.
+	char **names_pp = names; 
+
+	// Calculate the number of elements in the array ages.
 	int count = sizeof(ages) / sizeof(int);
 
-	// Display content of ages and names using indexes.
+	printf("Content of ages and names printed using array indexes.");
+	printf("\n");
 	for (size_t idx = 0; idx < count; idx++)
 	{
 		printf("%s is %d years old.", names[idx], ages[idx]);
@@ -18,6 +28,15 @@ int main(int argc, char *argv[])
 	}
 	printf("\n");
 	
+	printf("Content of ages and names printed using pointers.");
+	printf("\n");
+	for (size_t i = 0; i < count; i++)
+	{
+		printf("%s is %d years old.", *(names_pp + i), *(ages_p + i));
+		printf("\n");
+	}
+	printf("\n");
+
 	return EXIT_SUCCESS;
 }
 
